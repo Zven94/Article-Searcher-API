0735b5614d8365b8c5c5ff7d1119e3a156b71af2
Zven94 <nicobolanez@gmail.com>
2024-01-06 01:31:09 -0300

    fix linters issues

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,12 @@
 require 'net/http'
 
+# My controller for the search
 class SearchController < ApplicationController
   def index
     query = params[:q]
 
     results = fetch_data(query)
-    
+
     # save articles in an object
     @articles = process_results(results)
 
@@ -15,9 +16,9 @@ class SearchController < ApplicationController
   end
 
   def fetch_data(query)
-    base_url = "https://www.googleapis.com/customsearch/v1"
-    api_key = "AIzaSyBxDasvzampVLOi93azAijdfcr8XfUgJzk"
-    cx = "3700a2e0056ca4d7d"
+    base_url = 'https://www.googleapis.com/customsearch/v1'
+    api_key = 'AIzaSyBxDasvzampVLOi93azAijdfcr8XfUgJzk'
+    cx = '3700a2e0056ca4d7d'
 
     full_url = "#{base_url}?q=#{query}&key=#{api_key}&cx=#{cx}"
 
@@ -32,9 +33,10 @@ class SearchController < ApplicationController
   end
 
   def process_results(results)
-    items = results["items"]
+    items = results['items']
 
-    items_data = items&.map { |item| { "title" => item["title"], "snippet" => item["snippet"], "link" => item["link"] } } || []
-    items_data
+    items&.map do |item|
+      { 'title' => item['title'], 'snippet' => item['snippet'], 'link' => item['link'] }
+    end || []
   end
 end
