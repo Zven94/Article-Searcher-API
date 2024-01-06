925214babdfe7e6fbfbac95c543bb72bdfdacff1
Zven94 <nicobolanez@gmail.com>
2024-01-06 00:58:57 -0300

    added method to search_controller to fetcht data

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,2 +1,40 @@
+require 'net/http'
+
 class SearchController < ApplicationController
+  def index
+    query = params[:q]
+
+    results = fetch_data(query)
+    
+    # save articles in an object
+    @articles = process_results(results)
+
+    # return retults as a json
+
+    render json: @articles
+  end
+
+  def fetch_data(query)
+    base_url = "https://www.googleapis.com/customsearch/v1"
+    api_key = "AIzaSyBxDasvzampVLOi93azAijdfcr8XfUgJzk"
+    cx = "3700a2e0056ca4d7d"
+
+    full_url = "#{base_url}?q=#{query}&key=#{api_key}&cx=#{cx}"
+
+    uri = URI(full_url)
+    reponse = Net::HTTP.get(uri)
+    JSON.parse(reponse)
+
+    # in case of errors with the fetch:
+  rescue StandardError => e
+    Rails.logger.error("Error in the request... #{e.message}")
+    nil
+  end
+
+  def process_results(results)
+    items = results["items"]
+
+    items_data = items&.map { |item| { "title" => item["title"], "snippet" => item["snippet"], "link" => item["link"] } } || []
+    items_data
+  end
 end
