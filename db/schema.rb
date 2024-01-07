03e78595128dc0949f00079a2a3d36ff83599964
Nicolas Bolañez <Zven94>
2024-01-06 23:29:27 -0300

    created table for searched_terms

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,15 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 20_240_105_234_228) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_06_225556) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'articles', force: :cascade do |t|
-    t.string 'title'
-    t.text 'description'
-    t.string 'url'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
+  create_table "searched_terms", force: :cascade do |t|
+    t.string "term"
+    t.integer "searched_counter"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
