2c62fe91223a3a4508ae34eb1b9f70c465dfff3b
Nicolas Bolañez <Zven94>
2024-01-06 23:28:56 -0300

    searched_terms created to manage every search

---
+++ test/controllers/searched_terms_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class SearchedTermsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/models/searched_term_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class SearchedTermTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
